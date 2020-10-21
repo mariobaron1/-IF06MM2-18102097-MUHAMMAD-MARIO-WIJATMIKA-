450913309102ee67510e7f68f38ea7fe2aba01d1
mariobaron1 <69414741+mariobaron1@users.noreply.github.com>
2020-10-21 18:31:32 +0700

    menambahkan Empy Activity

---
+++ Tugas Pertemuan 3/HelloITTelkomPurwokerto/app/src/main/java/com/MuhammadMarioW_18102097/helloittelkompurwokerto/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.MuhammadMarioW_18102097.helloittelkompurwokerto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
