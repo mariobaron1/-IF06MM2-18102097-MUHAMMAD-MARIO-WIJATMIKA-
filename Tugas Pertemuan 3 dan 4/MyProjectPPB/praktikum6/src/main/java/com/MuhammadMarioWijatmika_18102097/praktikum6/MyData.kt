39893bccdf2552d3cb91861c4eefc03ae605f08c
mariobaron1 <69414741+mariobaron1@users.noreply.github.com>
2020-11-15 23:53:38 +0700

    Create itemlist and MyData

---
+++ Tugas Pertemuan 3 dan 4/MyProjectPPB/praktikum6/src/main/java/com/MuhammadMarioWijatmika_18102097/praktikum6/MyData.kt
@@ -0,0 +1,14 @@
+package com.MuhammadMarioWijatmika_18102097.praktikum6
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+class MyData {
+    @Parcelize
+    data class MyData(
+        var name: String,
+        var description: String,
+        var photo: String,
+        val : Parcelable
+    )
+}
