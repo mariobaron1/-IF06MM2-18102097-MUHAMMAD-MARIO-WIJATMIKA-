54f42410de1f6959383b0e1fd9f5064283ceeee3
mariobaron1 <69414741+mariobaron1@users.noreply.github.com>
2020-11-16 17:59:07 +0700

    Finish Recycleview Grid

--- Tugas Pertemuan 3 dan 4/MyProjectPPB/praktikum6/src/main/java/com/MuhammadMarioWijatmika_18102097/praktikum6/MyData.kt
+++ Tugas Pertemuan 3 dan 4/MyProjectPPB/praktikum6/src/main/java/com/MuhammadMarioWijatmika_18102097/praktikum6/MyData.kt
@@ -3,12 +3,9 @@ package com.MuhammadMarioWijatmika_18102097.praktikum6
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
-class MyData {
-    @Parcelize
-    data class MyData(
+@Parcelize
+data class MyData(
         var name: String,
         var description: String,
-        var photo: String,
-        val : Parcelable
-    )
-}
+        var photo: String
+): Parcelable
