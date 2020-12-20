d0d3169904a6023229c0c5f1df3db561adef902a
mariobaron1 <69414741+mariobaron1@users.noreply.github.com>
2020-12-20 20:20:04 +0700

    selesai mengerjakan praktikum 8

--- Tugas Pertemuan 3 dan 4/MyProjectPPB/praktikum8/src/main/java/com/MuhammadMarioWijatmika_18102097/praktikum8/SmsReceiverActivity.kt
+++ Tugas Pertemuan 3 dan 4/MyProjectPPB/praktikum8/src/main/java/com/MuhammadMarioWijatmika_18102097/praktikum8/SmsReceiverActivity.kt
@@ -2,6 +2,7 @@ package com.MuhammadMarioWijatmika_18102097.praktikum8
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sms_receiver.*
 
 class SmsReceiverActivity : AppCompatActivity() {
     companion object {
@@ -13,5 +14,12 @@ class SmsReceiverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sms_receiver)
+
+        title = getString(R.string.incoming_message)
+        val senderNo = intent.getStringExtra(EXTRA_SMS_NO)
+        val senderMessage = intent.getStringExtra(EXTRA_SMS_MESSAGE)
+        tv_from.text = getString(R.string.coming_from)+": "+senderNo
+        tv_message.text = senderMessage
+        btn_close.setOnClickListener { finish() }
     }
 }
